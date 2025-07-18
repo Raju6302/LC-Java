15a26ef7918fad59719e2018729ce2d3eec9892a
vijay jaripity <137879456+Raju6302@users.noreply.github.com>
2025-07-18 21:56:52 +0530

    Time: 1382 ms (5.09%), Space: 50.4 MB (41.81%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -3,7 +3,7 @@ class Solution {
         int n = nums.length;
         List<Integer> result = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            if (result.contains(nums[i])) continue;
+            if (result.size() == 0 || result.get(0) != nums[i]) {
                 int count = 0;
                 for (int j = 0; j < n; j++) {
                     if (nums[j] == nums[i]) {
@@ -11,6 +11,8 @@ class Solution {
                     }
                 }
                 if (count > n/3) result.add(nums[i]);
+            }
+            if (result.size() == 2) break;
         }
         return result;
     }

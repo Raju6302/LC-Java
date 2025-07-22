b9ec147a55b5972f5200e69bd6e940a6eb221db2
vijay jaripity <137879456+Raju6302@users.noreply.github.com>
2025-07-22 16:29:36 +0530

    Time: 24 ms (12.5%), Space: 58.5 MB (40.31%) - LeetHub

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int minDays(int[] bloomDay, int m, int k) {
+        int n = bloomDay.length;
+        long val =  (long)(m * k);
+        if (val > n) return -1;
+        int min = findMin(bloomDay);
+        int max = findMax(bloomDay);
+
+        int low = min, high = max, ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int result = findBloomDay(mid, bloomDay, m, k);
+
+            if (result >= m) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    int findBloomDay(int day, int[] nums, int m, int k) {
+        int count = 0; 
+        int bouquet = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= day) {
+                count++;
+            } else {
+                bouquet += (count / k) ;
+                count = 0;
+            }
+        }
+        bouquet += (count / k);
+        return bouquet;
+    }
+
+    int findMin(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            min = Math.min(min, nums[i]);
+        }
+        return min;
+    }
+
+    int findMax(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            max = Math.max(max, nums[i]);
+        }
+        return max;
+    }
+
+}
